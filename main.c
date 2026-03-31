96ab1173c7b06940d617f27d09db0df6a8cee30b
Kirill <salatmail86@gmail.com>
2026-03-31 11:41:17 +0700

    added_tests

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define LEN_P 11
+#define LEN_P 11 //13
 
 int div4(unsigned char* str){
 
@@ -43,8 +43,22 @@ int div4(unsigned char* str){
 int main()
 {
 
+  //делится на 4
     unsigned char P[LEN_P] = "1231214124\0";
+
+  //не делится на 4
+  //  unsigned char P[LEN_P] = "1231214125\0";
+
+  //не число
+  //  unsigned char P[LEN_P] = "123121412b\0";
+
+  //русские символы
+  //  unsigned char P[LEN_P] = "1231214124а\0";
+
     int res = div4(P);
+
+    //нулевой указатель
+    // int res = div(NULL);
 
     if (res != -1){
 
