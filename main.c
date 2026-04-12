107301813153a953396619e7c2962701a6df8aa3
Kirill <salatmail86@gmail.com>
2026-04-12 15:39:58 +0700

    workin_on_output_through_buffer

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 
+#define LEN 33
+
 int main()
 {
-    printf("Hello World!\n");
+    
+    char words[LEN] = "abc cba, bbc,ccb abc, abcd. ddc\0";
+
+    int i = 0;
+    int j = 0;
+
+    char buffer[1000] = {'0'};
+
+    while (words[i] != '\0' && words[i] != '.'){
+
+        if (words[i] == ' ' || words[i] == ','){
+
+            buffer[j+1] = '\0';
+            j = 0;
+
+            printf("%s\n", buffer);
+
+            while (words[i] == ' ' || words[i] == ','){
+                i++;
+            }
+
+        }
+        else{
+            buffer[j] = words[i];
+
+            i++;
+            j++;
+        }
+
+    }
+
+    printf("algorithm finished");
+    
     return 0;
+
 }
